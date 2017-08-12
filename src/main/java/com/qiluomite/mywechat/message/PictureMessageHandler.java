056be105c592eb4ea185f9798509858d89431e61
hexiangtao <798395170@qq.com>
2017-08-12 19:28:38 +0800

    新增语音消息自动下载功能

--- src/main/java/com/qiluomite/mywechat/message/PictureMessageHandler.java
+++ src/main/java/com/qiluomite/mywechat/message/PictureMessageHandler.java
@@ -1,15 +1,10 @@
 package com.qiluomite.mywechat.message;
 
-import java.io.File;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.blade.kit.FileKit;
-import com.blade.kit.http.HttpRequest;
 import com.blade.kit.json.JSONObject;
 import com.qiluomite.mywechat.bean.WechatMeta;
-import com.qiluomite.mywechat.util.PropertyReader;
 
 public class PictureMessageHandler extends AbstractMessageHandler {
 
@@ -23,13 +18,8 @@ public class PictureMessageHandler extends AbstractMessageHandler {
 	@Override
 	public void process(JSONObject msg) {
 		LOGGER.warn("开始处理图片消息");
-		PropertyReader pr=PropertyReader.load("classpath:config.properties");
-		String imgDir = pr.get("app.img_path");
+		String imgDir = meta.getConfig().get("app.media_path");
 		String msgId = msg.getString("MsgId");
-		FileKit.createDir(imgDir, false);
-		String imgUrl = meta.getBase_uri() + "/webwxgetmsgimg?MsgID=" + msgId + "&skey=" + meta.getSkey() + "&type=slave";
-		HttpRequest.get(imgUrl).header("Cookie", meta.getCookie()).receive(new File(imgDir + "/" + msgId + ".jpg"));
-		
-		
+		download(msg, MsgType.PICTURE, imgDir + "/" + msgId + ".jpg");
 	}
 }

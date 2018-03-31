7fca8b37da32ea1cfbb43c9261b6ec3673f8301b
hexiangtao <798395170@qq.com>
2018-03-31 10:52:01 +0800

    删除不相关代码

--- src/main/java/com/iyuexian/wechat4j/crawler/DocumentListener.java
+++
@@ -1,7 +0,0 @@
-package com.iyuexian.wechat4j.crawler;
-
-import org.jsoup.nodes.Document;
-
-public interface DocumentListener {
-	public void onDownload(String url, Document doc, LinkCollection collection);
-}

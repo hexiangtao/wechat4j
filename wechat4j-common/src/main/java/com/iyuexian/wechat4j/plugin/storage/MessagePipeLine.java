7c23be07685daaa8f2450a8624118c64e9d37b31
hexiangtao <798395170@qq.com>
2018-03-31 12:25:20 +0800

    update

--- wechat4j-common/src/main/java/com/iyuexian/wechat4j/plugin/storage/MessagePipeLine.java
+++
@@ -1,9 +0,0 @@
-package com.iyuexian.wechat4j.plugin.storage;
-
-import com.iyuexian.wechat4j.config.Enums.MsgType;
-import com.iyuexian.wechat4j.core.WechatMeta;
-
-public interface MessagePipeLine {
-	public void processs(WechatMeta meata, String line, MsgType msgType);
-
-}
--- wechat4j-common/src/main/java/com/iyuexian/wechat4j/util/Converter.java
+++
@@ -1,6 +0,0 @@
-package com.iyuexian.wechat4j.util;
-
-public interface Converter<S, T> {
-	public T convert(S s);
-
-}
